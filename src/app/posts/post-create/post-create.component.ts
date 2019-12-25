6286b63c4b95fea86401f8f15c54663d6c259f23
harika penukonda <penukondaharika@gmail.com>
2019-12-25 19:25:59 +0530

    adding src files

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent {
+  enteredValue = '';
+  newPost = 'NO CONTENT';
+  // onAddPost(postInput: HTMLTextAreaElement){
+
+    // alert('Post added');
+    // console.log(postInput);
+    // this.newPost = 'The user\'s post';
+    onAddPost() {
+      // this.newPost = postInput.value;
+      this.newPost = this.enteredValue;
+    }
+
+  }
+
