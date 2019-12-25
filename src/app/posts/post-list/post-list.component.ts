392bf66d5eef3a68c2bddb7107a3e425d4433e81
harika penukonda <penukondaharika@gmail.com>
2019-12-25 23:55:15 +0530

    Diving into Structural Directives
    added expansion panels

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -8,9 +8,10 @@ import { Component } from '@angular/core';
 
 
 export class PostListComponent {
-    posts = [
-        {title: 'First Post', content: 'This is the first post\'s content'},
-        {title: 'Second Post', content: 'This is the Second post\'s content'},
-        {title: 'Third Post', content: 'This is the Third post\'s content'}
-    ];
+  //  posts = [
+  //      {title: 'First Post', content: 'This is the first post\'s content'},
+  //      {title: 'Second Post', content: 'This is the Second post\'s content'},
+  //      {title: 'Third Post', content: 'This is the Third post\'s content'}
+  //  ];
+  posts = [];
 }
